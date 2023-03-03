abcf3bd19df925c23c08402f49302b04ff419b3f
Jack Stubblefield <jStubblefield@lbisit.onmicrosoft.com>
2023-03-02 21:21:40 -0600

    basic map working, removed strict mode from index

--- src/components/Eclipse/Eclipse.js
+++ src/components/Eclipse/Eclipse.js
@@ -1,12 +1,36 @@
 import React from 'react';
-
+import GoogleMapReact from 'google-map-react';
 import './Eclipse.css';
+import EclipseMap from './EclipseMap';
+const MAP_KEY = process.env.REACT_APP_MAP_KEY;
 
+
+const defaultProps = {
+    center: {
+        lat: 10.99835602,
+        lng: 77.01502627
+    },
+    zoom: 11
+};
 class Eclipse extends React.Component {
+
     render() {
+
         return (
             <>
-                <p>This is the Eclipse Page</p>
+                <div style={{ height: '100vh', width: '100%' }}>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: MAP_KEY }}
+                        defaultCenter={defaultProps.center}
+                        defaultZoom={defaultProps.zoom}
+                    >
+                        <EclipseMap
+                            lat={59.955413}
+                            lng={30.337844}
+                            text="My Marker"
+                        />
+                    </GoogleMapReact>
+                </div>
             </>
         );
     }
