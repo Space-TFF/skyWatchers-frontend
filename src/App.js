3b35f7e4d935988a5c3ba63b8b2c9dbb9a8911e5
SHargus <87950738+harguss@users.noreply.github.com>
2023-03-03 13:55:06 -0600

    added images and cards

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from './components/Footer/Footer.js';
 import { Outlet } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+let SERVER = process.env.REACT_APP_SERVER;
+
 let AUTHSERVER = process.env.REACT_APP_DOMAIN;
 let yourClientId = process.env.REACT_APP_CLIENTID;
 
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,137 @@
 import React from 'react';
 
 import './Aboutus.css';
+import EvaGrace from '../img/EvaGrace.jpg'
+import SusanHargus from '../img/SusanHargus.jpg'
+import MikePace from '../img/MikePace.jpeg'
+import JackStubblefield from '../img/JackStubblefield.jpeg'
 
 class Aboutus extends React.Component {
+  constructor(props) {
+    super(props);
+    this, state = {
+
+    };
+  }
   render() {
     return (
       <>
-        <p>This is the Aboutus Page</p>
+        <div className='aboutUs'>
+          <div className='cardWrap'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <div className='photoCard'>
+                    <img src={EvaGrace} alt="Eva Grace"
+                      className='photoImage'
+                    // style={{width:'60%',
+                    // height:'70%',
+                    // padding:'15%'}}
+                    />
+                    <h1>Eva Grace Smith</h1>
+                  </div>
+                </div>
+                <div className="flip-card-back">
+                  <div className='textBox'>
+                    <p>Full Stack Software Developer</p>
+                    <p>Using my love of web design, art, and people to make the world a better place, one page at a time.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <div className='photoCard'>
+                    <img src={MikePace} alt="Mike Pace"
+                      className='photoImage'
+                    // style={{width:'60%',
+                    // height:'70%',
+                    // padding:'15%'}}
+                    />
+                    <h1>Mike Pace</h1>
+                  </div>
+                </div>
+                <div className="flip-card-back">
+                  <div className='textBox'>
+                    <p>Full Stack Software Developer</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <div className='photoCard'>
+                    <img src={MikePace} alt="Mike Pace"
+                      className='photoImage'
+                    // style={{width:'60%',
+                    // height:'70%',
+                    // padding:'15%'}}
+                    />
+                    <h1>Mike Pace</h1>
+                  </div>
+                </div>
+                <div className="flip-card-back">
+                  <div className='textBox'>
+                    <p>Full Stack Software Developer</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <div className='photoCard'>
+                    <img src={SusanHargus} alt="Susan Hargus"
+                      className='photoImage'
+                    // style={{width:'60%',
+                    // height:'70%',
+                    // padding:'15%'}}
+                    />
+                    <h1>Susan Hargus</h1>
+                  </div>
+                </div>
+                <div className="flip-card-back">
+                  <div className='textBox'>
+                    <p>Full Stack Software Developer</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <div className='photoCard'>
+                    <img src={JackStubblefield} alt="Jack Stubblefield"
+                      className='photoImage'
+                    // style={{width:'60%',
+                    // height:'70%',
+                    // padding:'15%'}}
+                    />
+                    <h1>Jack Stubblefield</h1>
+                  </div>
+                </div>
+                <div className="flip-card-back">
+                  <div className='textBox'>
+                    <p>Full Stack Software Developer</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </>
     );
   }
