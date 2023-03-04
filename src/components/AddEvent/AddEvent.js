033a0216bbcab1f3547edc63c0cd4fe8c3d2c9a0
EvaGraceSmith <smithevagrace@gmail.com>
2023-03-04 11:32:28 -0600

    created addEvent dialog

---
+++ src/components/AddEvent/AddEvent.js
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add Event</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To add an event to this location, please fill out the following information.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Location Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Name of Event (optional)"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+                    <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="City"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+                    <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="State"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+                              <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Date of Arrival"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+                    <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+              <FormGroup>
+
+      <FormControlLabel control={<Checkbox />} label="Make Public" />
+    </FormGroup>
+
+        </DialogContent>
+        <DialogActions>
+          
+
+          <Button onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
