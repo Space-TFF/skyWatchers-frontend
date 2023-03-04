3688bccaf6ac9261e9f9be69a1cc563169cce611
Jack Stubblefield <jStubblefield@lbisit.onmicrosoft.com>
2023-03-04 11:41:40 -0600

    removed unnessecary state from marker component

--- src/components/EventMarker/EventMarker.js
+++ src/components/EventMarker/EventMarker.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
+import './EventMarker.css'
 
 
 /**
  *! Text will essentially be the "Marker" for the map.
  */
 class EventMap extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: '',
 
-        }
-    }
     render() {
-        return (<div>{this.props.text}</div>);
+        return (<div className='eventMarker'>{this.props.text}</div>);
     }
 }
 
