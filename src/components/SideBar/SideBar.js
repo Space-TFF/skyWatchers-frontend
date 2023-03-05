eb3e1df17f7a30e49ac683a3f2620d47f2cf9f66
EvaGraceSmith <smithevagrace@gmail.com>
2023-03-04 20:11:19 -0600

    created SideBar

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="Eclipse Event"
+      />
+      <CardContent>
+        <Typography id="name" gutterBottom variant="h5" component="div">
+          Name of Event
+        </Typography>
+        <Typography id="description" variant="body2" color="text.secondary">
+Join us for this amazing event. Wide panoramic views. Live Music! Vendors include Red Bull and SpaceX.
+        </Typography>
+
+        <Typography id="city" gutterBottom variant="subtitle2" component="div">
+          City
+        </Typography>
+        <Typography id="state" gutterBottom variant="subtitle2" component="div">
+          State
+        </Typography>
+        <Typography gutterBottom variant="subtitle2" component="div">
+          Date
+        </Typography>
+        <Typography id="additionalInfo"variant="body2" color="text.secondary">
+Additional Info
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add to My Events</Button>
+
+      </CardActions>
+    </Card>
+
+
+    </Box>
+  );
+
+  return (
+    <div>
+      {['left'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
