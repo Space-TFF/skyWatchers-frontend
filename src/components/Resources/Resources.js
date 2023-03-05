b575c6ef7a0976e8128b42c36bd8eaaebaef3f41
SHargus <87950738+harguss@users.noreply.github.com>
2023-03-05 13:09:09 -0600

    readded images to the files

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -2,21 +2,72 @@ import React from 'react';
 
 import './Resources.css';
 import { Container } from 'react-bootstrap';
-import eclipseBandW from '../../img/eclipseBandW.jpg';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import eclipseBandW2 from '../../img/eclipseBandW2.jpg';
+import kids from '../../img/kids.jpg';
+import tshirt from '../../img/tshirt.jpg';
+import glasses from '../../img/glasses.jpg';
 
 class Resources extends React.Component {
   render() {
+
     return (
 
- <Container>
-  <>
-  <img src={eclipseBandW} alt="eclipseBandW" className="eclipseBandW"/>
+      <div className="page">
+        <img src={eclipseBandW2} alt="eclipseBandW2" className="eclipseBandW2" />
 
-<p>This is the Resources Page</p>
-</>
-</Container>    
+        <Container className="products">
+          <Card style={{ width: '22rem' }}>
+            <Card.Img variant="top" src={kids} alt="kids" className="kids" />
+            <Card.Body>
+              <Card.Title>Safety</Card.Title>
+              <Card.Text>
+              Protect your eyes. Except during the brief total phase of a total solar eclipse, when the Moon completely blocks the Sun’s bright face, it is not safe to look directly at the Sun without specialized eye protection for solar viewing.
+              </Card.Text>
+              {/* <Button variant="primary" onClick={() => window.open('https://www.solareclipsetimer.com/index.html', '_blank')}>
+                Resources
+              </Button> */}
+              <Button variant="primary" onClick={() => window.open('https://solarsystem.nasa.gov/eclipses/safety/', '_blank')}>
+                Safety
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '22rem' }}>
+            <Card.Img variant="top" src={tshirt} alt="tshirt" className="tshirt" />
+            <Card.Body>
+              <Card.Title>T-Shirts</Card.Title>
+              <Card.Text>100% cotton t-shirt has our signature glow-in-the-dark solar corona on the front, and the April 8, 2024 concert-style Great North American Eclipse "Path of Totality" on the back.($25.00)
+              </Card.Text>
+              <Button variant="primary" onClick={() => window.open('https://www.greatamericaneclipse.com/apparel/2024-glow-in-the-dark-corona-shirt-with-concert-style-back-mens', '_blank')}>
+                Buy
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '22rem' }}>
+            <Card.Img variant="top" src={glasses} alt="glasses" className="glasses" />
+            <Card.Body>
+              <Card.Title>Gear</Card.Title>
+              <Card.Text>
+                Solar Eclipse Glasses Safe Solar Shades - Pack of 4 $14.49 ($3.62$3.62 / Count) white cardboard - pack of 4 CE certified
+              </Card.Text>
+
+
+              <Button variant="primary" onClick={() => window.open('https://a.co/d/9Vh695p', '_blank')}>
+                Buy
+              </Button>
+
+            </Card.Body>
+          </Card>
+        </Container>
+      </div>
     );
+
+
+
+
   }
 }
+
 
 export default Resources;
