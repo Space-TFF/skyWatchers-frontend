30290bb41ec9f2e6c9be6b4f81bf921c8ffce681
Jack Stubblefield <Jack011601@gmail.com>
2023-03-06 13:48:48 -0600

    centered map, updated env sample

--- src/components/Eclipse/Eclipse.js
+++ src/components/Eclipse/Eclipse.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { Container } from '@mui/material';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: '50vw',
+  height: '80vh'
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
+  lat: 41.97111725808147,
+  lng: -91.6563726642888,
 };
 
 class Eclipse extends Component {
@@ -17,14 +18,17 @@ class Eclipse extends Component {
       <LoadScript
         googleMapsApiKey={process.env.REACT_APP_MAP_KEY}
       >
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-        >
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></>
-        </GoogleMap>
+        <Container>
+
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+          >
+            { /* Child components, such as markers, info windows, etc. */}
+            <></>
+          </GoogleMap>
+        </Container>
       </LoadScript>
     )
   }
