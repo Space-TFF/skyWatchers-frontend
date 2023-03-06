ce9c2884cd97bffe22c3edb5c1b7b8b83eaf946c
EvaGraceSmith <smithevagrace@gmail.com>
2023-03-05 19:13:09 -0600

    open and close button moved to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,21 +3,42 @@ import SelectEventCard from "../Eclipse/SelectEventCard";
 import SideDrawer from "../Eclipse/SideDrawer.js";
 import './Home.css';
 import AddEvent from "../Eclipse/AddEvent.js"
+import Button from '@mui/material/Button';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      open: false,
+    }};
+
+  handleClickOpen = () => {
+    this.setState({open:true});
+  };
+  handleClickClose = () => {
+    this.setState({open:false})
+  };
+
   render() {
     return (
       <>
         <div>
+
           <SelectEventCard
             name="Event Name"
             description=" Join us for this amazing event. Wide panoramic views. Live Music! Vendors include Red Bull and SpaceX." />
+
+<Button variant="outlined" onClick={this.handleClickOpen}>
+        Open form dialog
+      </Button>
             <AddEvent
             name="Event Name"
             description="Description"
             city="City"
             state="State"
             email="email"
+            open={this.state.open}
+            handleClickClose={this.handleClickClose}
             />
           <SideDrawer
           name="Event Name"
