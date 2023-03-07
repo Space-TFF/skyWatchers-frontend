1490877f9bc22c86666416f526147ea390665fa3
SHargus <87950738+harguss@users.noreply.github.com>
2023-03-07 12:46:22 -0600

    added info and links for campgrounds

--- src/components/About/CampSeedInfo/CampSeed.js
+++ src/components/About/CampSeedInfo/CampSeed.js
@@ -6,7 +6,7 @@
 
 
 
-
+<p>
 image:
 name: "Hidden Falls Adventure Park",
   description:"2023 CAMPSPOT AWARDS WINNER: Best Campgrounds for Weekenders! If you enjoy off-roading in a dirtbike, ATV/UTV, or full-size vehicle, then you will love camping at Hidden Falls Adventure Park! Offering cabins, RV, and primitive tent sites, multi-day passes for riding: for every night that you camp in the park, you will be charged riding fees for the day before and the day after. These fees apply to both the driver and passenger and range from $15-$55 depending on stay. The price is well worth it with over 2,000 acres of off-road perfection! If you need to rent an off-road vehicle, Hill Country Adventure Rentals is recommended, as Hidden Falls does not provide rentals. The nearest gas station is 7 miles from the park. Onsite you will find bathrooms, showers, and a general store. Gear up and head out to Hidden Falls Adventure Park!" ,
@@ -54,65 +54,67 @@ lon:'',
 
 
 image:
-name: "",
-  description:"" ,
-  city:"",
-  state:"",
+name: "Black River Wine House & RV Retreat",
+  description:"Nestled in the majestic heart of the hills, you’ll find the perfect place to meet friends, listen to live music, and feel the breeze as you sip your many choices of Missouri wine and craft beer selections. This perfect backdrop allows you to sit back and unwind, taking in the beautiful views of this gorgeous countryside. You'll hear easy listening, acoustic varieties of live music, provided on Saturday and Sunday afternoons for your pleasure. Make Black River your next getaway destination!" ,
+  city:"50129 Highway 49, Annapolis,",
+  state:"MO",
   email:"",
-additionalInfo:"", (this would be the link to the camp website)
+additionalInfo:"https://www.campspot.com/park/black-river-wine-house-and-rv-retreat", (this would be the link to the camp website)
 lat:"",
 lon:'',
 
 
 image:
-name: "",
-  description:"" ,
-  city:"",
-  state:"",
+name: "Otahki Lake Cabins & Campground",
+  description:"2023 CAMPSPOT AWARDS WINNER: Best in America! If you're looking for the perfect family-friendly getaway, then look no further than Otahki Lake Cabins & Campground! Located in the heart of the Missouri Ozarks, Otahki Lake is just a short drive away from St. Louis, MO(2hrs) and Memphis, TN (3hrs). This 1200-acre park features a 23-acre lake and 2 miles of river frontage, 15 Deluxe Cabins w/hot tubs, 42 Full-hookup RV Sites, 4 Covered Wagons, 8 Glamping Tents, 8 Primitive Cabins, and 2 tiny Sunset Cabins. Rent an E-bike or Golf cart and ride the over 8 miles of gravel roads. Kayak or canoe on the lake or river, you can also enjoy floats and rafting trips ranging from 1 mile to 10 miles on the picturesque St. Francois River. Relax on the river gravel bar or at the lake's swim beach. Join Otahki Lake Cabins & Campground for one of the many themed weekends and make the most of your family camping trip! Book your spot today!" ,
+  city:"1224 Wayne 318, Patterson",
+  state:"MO",
   email:"",
-additionalInfo:"", (this would be the link to the camp website)
+additionalInfo:"https://www.campspot.com/park/otahki-lake-cabins-and-campground?location=Annapolis,%20MO&latitude=37.38525&longitude=-90.690629&adults=2&children=0&pets=0", (this would be the link to the camp website)
 lat:"",
 lon:'',
 
 
 image:
-name: "",
-  description:"" ,
-  city:"",
-  state:"",
+name: "Bill Monroe Music Park and Campground",
+  description:"Get outside, disconnect the phones and reconnect with nature at Bill Monroe's Music Park and Campground. Sit by the crackling fire under a star-filled sky with the sight and sounds of nature filling your ears. Known for some of the best bluegrass festivals in the country, the history Bill Monroe Music Park & Campground is also one of the largest campgrounds in southern Indiana. Whether you're an avid hiker or a relaxed fisher, there is something to do at Bill Monroe!" ,
+  city:"5163 N State Road 135, Morgantown",
+  state:"IN",
   email:"",
-additionalInfo:"", (this would be the link to the camp website)
+additionalInfo:"https://www.campspot.com/park/bill-monroe-music-park-campground", (this would be the link to the camp website)
 lat:"",
 lon:'',
 
 
 image:
-name: "",
-  description:"" ,
-  city:"",
-  state:"",
+name: "Off The Hook Campground",
+  description:"You are now Off The Hook! Sites with full hook-ups and access to great fishing & swimming during your stay, this relaxed atmosphere for family and friends is guaranteed enjoyment!
+" ,
+  city:"11760 State Route 122, Camden",
+  state:"OH",
   email:"",
-additionalInfo:"", (this would be the link to the camp website)
+additionalInfo:"https://www.campspot.com/park/off-the-hook-campground", (this would be the link to the camp website)
 lat:"",
 lon:'',
  
 image:
-name: "",
-  description:"" ,
-  city:"",
-  state:"",
+name: "Niagara Shores Campground and Conference Center",
+  description:"Niagara Shores specializes in overnight lodging and camping or glamping. With plenty of spaces, a large old 5000 square ft. Tabernacle, a Wood land chapel, 10,000 square ft. Convention Center with a commercial kitchen, a beautiful lakeside dining room, miniature golf, swimming pool, clean community bathrooms, souvenir shop, grocery store, snack bar and much more, this is truly a unique must-stay in the Niagara region. With the ability to accommodate up to 250 guests per night in full capacity, you'll find the perfect spot for you! Not to mention, an awesome 1.5 mile nature walk for meditation, solace and may feature extremely fun themes in the future!" ,
+  city:"6419 Lake Rd, Appleton",
+  state:"NY",
   email:"",
-additionalInfo:"", (this would be the link to the camp website)
+additionalInfo:"https://www.campspot.com/park/niagara-shores-campground-conference-center", (this would be the link to the camp website)
 lat:"",
 lon:'',
 
 image:
-name: "",
-  description:"" ,
-  city:"",
-  state:"",
+name: "Port Bay Campground",
+  description:"Port Bay Campground is a family owned and operated campground in Wolcott, New York. When you visit, you'll become a part of the family with exciting activities spread throughout open season. Enjoy the surrounding nature views, the peaceful atmosphere, local attractions, and so much more. Book your spot today and make Port Bay Campground your next getaway destination!" ,
+  city:"8346 East Port Bay Road, Wolcott",
+  state:"NY",
   email:"",
-additionalInfo:"", (this would be the link to the camp website)
+additionalInfo:"https://www.campspot.com/park/port-bay-campground-wolcott-ny", (this would be the link to the camp website)
 lat:"",
 lon:'',
 
+</p>
